56efcfffc0236c436eba47e95bae2ccb8324e94e
NRGoranov <nrgtrwsales@gmail.com>
2024-10-19 14:26:53 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running ${port}`);
+});

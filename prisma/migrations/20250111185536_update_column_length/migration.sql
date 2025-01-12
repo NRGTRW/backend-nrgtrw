d2845fdd2881b9e153f91f50500ce9cae1c9db87
NRG <nikolay.r.goranov.2020@elsys-bg.org>
2025-01-12 17:03:18 +0200

    Updated migration for column length changes

--- prisma/migrations/20250111185536_update_column_length/migration.sql
+++ prisma/migrations/20250111185536_update_column_length/migration.sql
@@ -1,6 +1,4 @@
--- AlterTable
-ALTER TABLE `product` MODIFY `imageUrl` TEXT NULL;
+ALTER TABLE product MODIFY imageUrl TEXT NULL;
 
--- AlterTable
-ALTER TABLE `productcolor` MODIFY `imageUrl` TEXT NOT NULL,
-    MODIFY `hoverImage` TEXT NOT NULL;
+ALTER TABLE productcolor MODIFY imageUrl TEXT NOT NULL,
+    MODIFY hoverImage TEXT NOT NULL;
